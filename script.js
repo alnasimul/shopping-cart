24de4afa4a4d7d992efa1f58995101b4b62a9664
alnasimul <alnasimul@gmail.com>
2020-03-26 23:09:29 +0600

    this is our third commit about remove item event handler

--- script.js
+++ script.js
@@ -3,6 +3,23 @@
 var plus1Btn = document.querySelector("#plus1");
 var minus1Btn = document.querySelector("#minus1");
 var getPrice1 = parseInt(document.querySelector("#getPrice1").innerText);
+var input1 = document.querySelector("#valueChanger1");
+
+input1.addEventListener("keyup",function(event){
+    enterValue("#valueChanger1","#getPrice1",1219);
+    subtotalAndTotal("#subtotal");
+    subtotalAndTotal("#total");
+});
+
+function enterValue(quantity,unitPrice,dPrice){
+    if(event.keyCode === 13){
+        let valuePassed = parseFloat(document.querySelector(quantity).value);
+        let updatePrice1 = valuePassed * dPrice;
+        document.querySelector(unitPrice).innerText = updatePrice1;
+
+    }
+}
+
 
 plus1Btn.addEventListener("click",function(){
     let quantity = increment1("#valueChanger1");
@@ -49,6 +66,14 @@ function decrement1(quantity){
 var plus2Btn = document.querySelector("#plus2");
 var minus2Btn = document.querySelector("#minus2");
 var getPrice2 = parseInt(document.querySelector("#getPrice2").innerText);
+var input2 = document.querySelector("#valueChanger2");
+
+input2.addEventListener("keyup",function(event){
+    enterValue("#valueChanger2","#getPrice2",59);
+    subtotalAndTotal("#subtotal");
+    subtotalAndTotal("#total");
+});
+
 
 plus2Btn.addEventListener("click",function(){
     let quantity = increment2("#valueChanger2");
@@ -104,5 +129,18 @@ function sumUp(product1, product2){
     return subtotal ;
 }
 
+// remove item event handler
 
+var item1 = document.querySelector("#item1");
+var item2 = document.querySelector("#item2");
+
+item1.addEventListener("click",function(){
+    let cart1 = document.querySelector("#cart1");
+    cart1.style.display = "none";
+});
+
+item2.addEventListener("click",function(){
+    let cart2 = document.querySelector("#cart2");
+    cart2.style.display = "none"
+});
 
